b0deb14d52f89e2815a18c92f8a5dfb5ce640330
Kaloyan Stoyanov <37839872+kalostoyanov@users.noreply.github.com>
2018-04-06 15:52:39 +0200

    Update main.py
    Added docstrings

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ words_in_the_line = []
 
 
 def read_dict():
-
+    """Check if dictionary.txt is present and read it into a list."""
     try:
         with open("dictionary.txt") as dictionary:
-
             for dictionary_line in iter(dictionary): # this whole block of code generates two lists, containing either abbreviation, or translation, but either way on the same position aka they're matching
                 words_in_the_line = dictionary_line.split()
                 all_abbreviations.append(words_in_the_line[0]) # this adds just the first word to all_abbreviations
@@ -25,12 +24,18 @@ def read_dict():
 
 
 def compare_dict(new_text,all_abbreviations,all_translations):
+    """Compare text message with dictionary entries.
+    
+    Keyword args:
+    new_text --- list with orignal text message generated through user input
+    all_abbreviations --- list with abbreviations generated through read_dict()
+    all_translations --- list with translations generated through read_dict()
+    """
     final_message = ""
     for word_position in range(0,len(new_text)):
         current_word = new_text[word_position]
         try:
             match_position = all_abbreviations.index(current_word.upper()) # i think this will execute only if current_word (str) is in all_abbreviations and it will retrn the index number
-
             final_message = final_message + " " + all_translations[match_position] # all_translation[match_position] makes sures it retrieves the corresponding translation - as we already matched abbreviations with translations
         except:
             final_message = final_message + " " + current_word
